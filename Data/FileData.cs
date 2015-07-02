b4a43ed6c8e3b001c5f7ac0014895b9a2e66f56f
Jean-Pierre Bouchard <jeanpierre.bouchard80@gmail.com>
2015-07-02 00:58:48 -0400

    setting inherite from FileData and serialization working with a XMLFormatter

---
+++ Data/FileData.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AppLib.Formatter;
+using System.Reflection;
+using System.IO;
+
+namespace AppLib.Data
+{
+    public class FileData
+    {
+        private string m_sFilename = "";
+        private XMLFormatter m_xmlFormatter;
+
+        public FileData()
+        {
+            m_xmlFormatter = new XMLFormatter();
+        }
+        /**
+         * @return false is load nothing
+         * 
+         */
+        public bool Load()
+        {
+            if (File.Exists(m_sFilename))
+            {
+                MethodInfo method = m_xmlFormatter.GetType().GetMethod("load");
+                MethodInfo genericMethod = method.MakeGenericMethod(this.GetType());
+                object[] parametersArray = new object[] { m_sFilename };
+                FileData result = (FileData)genericMethod.Invoke(m_xmlFormatter, parametersArray);
+                Type t = result.GetType();
+                foreach (PropertyInfo propertyInfo in t.GetProperties())
+                {
+                    if (propertyInfo.CanRead)
+                    {
+                        object value = propertyInfo.GetValue(result, null);
+                        propertyInfo.SetValue(this, value, null);
+                    }
+                }
+                return true;
+            }
+            return false
+        }
+        public void write()
+        {
+            MethodInfo method = m_xmlFormatter.GetType().GetMethod("write");
+            MethodInfo genericMethod = method.MakeGenericMethod(this.GetType());
+            object[] parametersArray = new object[] { m_sFilename, this };
+            genericMethod.Invoke(m_xmlFormatter, parametersArray);
+        }
+
+        #region ACCESSORS
+
+        public string FILENAME
+        {
+            get
+            {
+                return m_sFilename;
+            }
+            set
+            {
+                m_sFilename = value;
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ Formatter/XMLFormatter.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace AppLib.Formatter
+{
+    class XMLFormatter
+    {
+        #region XML
+
+        public T load<T>(string sFilename) where T : new()
+        {
+            T o = new T();
+            XmlSerializer xs = new XmlSerializer(o.GetType());
+
+            if (File.Exists(sFilename))
+            {
+                using (StreamReader fr = new StreamReader(sFilename))
+                {
+                    T data = (T)xs.Deserialize(fr);
+                    return data;
+                }
+            }
+            else
+            {
+                string directory = Path.GetDirectoryName(sFilename);
+                if (!Directory.Exists(directory))
+                {
+                    Directory.CreateDirectory(directory);
+                }
+            }
+            return o;
+        }
+
+        public void write<T>(string sFilename, object file)
+        {
+            XmlSerializer xs = new XmlSerializer(file.GetType());
+            using (StreamWriter fw = new StreamWriter(sFilename))
+            {
+                xs.Serialize(fw, file);
+            }
+        }
+        #endregion
+    }
+}
