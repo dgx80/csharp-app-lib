c4b6c711578359d50d400ee968c15be67fd06e2f
Jean-Pierre Bouchard <jeanpierre.bouchard80@gmail.com>
2015-07-25 21:45:31 -0400

    refactor and encapsulate in ApplicationSaveFileDialog

--- Forms/ApplicationForm.cs
+++ Forms/ApplicationForm.cs
@@ -46,47 +46,12 @@ namespace AppLib.Forms
         /// <summary>
         /// when a new project is called..from menustrip of shortcut
         /// </summary>
-        /// <param name="defaultFileName">when you want to load a default file name</param>
-        public void OnNewProject(string defaultFileName = "")
+        public void OnNewProject()
         {
-            SaveFileDialog sfd = new SaveFileDialog();
-            string ext = m_applicationManager.APP_DATA.SETTINGS.getProjectFileExtension();
-            sfd.DefaultExt = ext;
-            sfd.AddExtension = true;
-            sfd.Filter = "Files (*." + ext + "*)|*." + ext + "*";
-            string s = m_applicationManager.APP_DATA.CURRENT_PROJECT_NAME;
-
-            if (s != "")
+            string fullName = APP_SAVE_FILE_DIALOG.askAFileName();
+            if (fullName != "")
             {
-                System.IO.FileInfo fileInfo = new System.IO.FileInfo(s);
-                if (System.IO.Directory.Exists(fileInfo.DirectoryName))
-                {
-                    sfd.InitialDirectory = fileInfo.DirectoryName;
-                }
-            }
-            if (defaultFileName != "")
-            {
-                sfd.FileName = defaultFileName;
-            }
-            if (sfd.ShowDialog() == DialogResult.OK)
-            {
-                string fullName = sfd.FileName;
-                //validate if the extension is authorised
-                FileInfo fi = new FileInfo(fullName);
-                string currentExtention = fi.Extension;
-                string expectedExtension = "." + ext;
-                if (currentExtention == expectedExtension)
-                {
-                    m_applicationManager.onNewProjectFile(fullName);
-                    return;
-                }
-                else
-                {
-                    // reload the saveFileDialog with the same name and extention is replaced
-                    MessageBox.Show("only (*." + ext + ") extention is authorized as project format");
-                    string fileName = fi.Name.Replace(currentExtention, expectedExtension);
-                    OnNewProject(fileName);
-                }
+                m_applicationManager.onNewProjectFile(fullName);
             }
         }
         /// <summary>
@@ -117,7 +82,15 @@ namespace AppLib.Forms
 
         #region PROPERTIES
 
-
+        public ApplicationSaveFileDialog APP_SAVE_FILE_DIALOG
+        {
+            get
+            {
+                string ext = m_applicationManager.APP_DATA.SETTINGS.getProjectFileExtension();
+                string currentProjectName = m_applicationManager.APP_DATA.CURRENT_PROJECT_NAME;
+                return new ApplicationSaveFileDialog(ext, currentProjectName);
+            }
+        }
 
         #endregion
 
---
+++ Forms/ApplicationSaveFileDialog.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace AppLib.Forms
+{
+    public class ApplicationSaveFileDialog
+    {
+        private string m_authorisedEntension;
+        private string m_currentWorkingDirectory = "";
+
+        public ApplicationSaveFileDialog(string authorisedEntension, string currentProjectName = "")
+        {
+            m_authorisedEntension = authorisedEntension;
+            string s = currentProjectName;
+            if (s != "")
+            {
+                System.IO.FileInfo fileInfo = new System.IO.FileInfo(s);
+                if (System.IO.Directory.Exists(fileInfo.DirectoryName))
+                {
+                    m_currentWorkingDirectory = fileInfo.DirectoryName;
+                }
+            }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="defaultFileName">
+        /// when you want to load a default file name,
+        /// because this function is recucive when you have an error with unauthoriezed extention
+        /// </param>
+        /// <returns>
+        /// fullname of file or an empty string when is interrupted
+        /// </returns>
+        public string askAFileName(string defaultFileName = "")
+        {
+            string fullName = "";
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.DefaultExt = m_authorisedEntension;
+            sfd.AddExtension = true;
+            sfd.Filter = "Files (*." + m_authorisedEntension + "*)|*." + m_authorisedEntension + "*";
+
+            if (m_currentWorkingDirectory != "")
+            {
+                sfd.InitialDirectory = m_currentWorkingDirectory;
+            }
+            if (defaultFileName != "")
+            {
+                sfd.FileName = defaultFileName;
+            }
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                fullName = sfd.FileName;
+                //validate if the extension is authorised
+                FileInfo fi = new FileInfo(fullName);
+                string currentExtention = fi.Extension;
+                string expectedExtension = "." + m_authorisedEntension;
+                if (currentExtention == expectedExtension)
+                {
+                    return fullName;
+                }
+                else
+                {
+                    // reload the saveFileDialog with the same name and extention is replaced
+                    MessageBox.Show("only (*." + m_authorisedEntension + ") extention is authorized as project format");
+                    string fileName = fi.Name.Replace(currentExtention, expectedExtension);
+                    return askAFileName(fileName);
+                }
+            }
+            return "";
+        }
+    }
+}
