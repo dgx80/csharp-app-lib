596e82ebe2eed75ec5caa80a16ff8afafc7ca72e
Jean-Pierre Bouchard <jeanpierre.bouchard80@gmail.com>
2015-07-26 21:50:15 -0400

    remove some useless namespace

--- ApplicationManager.cs
+++ ApplicationManager.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using AppLib.Data;
+﻿using AppLib.Data;
 using System.Reflection;
 using System.IO;
 using AppLib.Components;
